dcecc2c8eb7dce0792625cb936c8df52c4d7f072
snupa <adrian@clym.io>
2024-09-11 17:17:59 +0300

    tweak config

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = function init(thorin, opt, pluginName) {
     },       // this is the service name
     token: process.env.CLUSTER_TOKEN || null, // A shared verification token used for auth
     tokenHeader: 'x-cluster-token',         // The HTTP header to place the auth token in
-    namespace: 'svc.cluster.local',         // The kube namespace name
+    namespace: process.env.CLUSTER_NAMESPACE || 'svc.cluster.local',         // The kube namespace name
     protocol: 'http',               // The default protocol for all communication
     alias: {  // aliases for services.
     },
